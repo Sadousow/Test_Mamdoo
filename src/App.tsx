aa171cfbd064fba86fc10bc9b2c1fe9ac103538f
Sadousow <118399556+Sadousow@users.noreply.github.com>
2024-01-10 21:57:01 +0100

    Mamdoo

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Admin, Resource, ShowGuesser } from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./composants/user";
+import { PostList, PostEdit, PostCreate } from "./composants/posts";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+
+// const newLocal = "name";
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    {/* <Resource name="users" list={UserList} recordRepresentation={newLocal} /> */}
+    <Resource
+      name="users"
+      list={UserList}
+      show={ShowGuesser}
+      recordRepresentation="name"
+      icon={UserIcon}
+    />
+  </Admin>
+);
