e105800a3b7147fffd24f313bcfbd146773bed57
Sadousow <sowsadou615@gmail.com>
2025-02-27 20:00:38 +0100

    Replace commented-out Resource components with a single Resource for UserList in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,6 @@ import UserIcon from "@mui/icons-material/Group";
 // const newLocal = "name";
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    {/* <Resource
-      name="posts"
-      list={PostList}
-      edit={PostEdit}
-      create={PostCreate}
-      icon={PostIcon}
-    /> */}
-    {/* <Resource name="users" list={UserList} recordRepresentation={newLocal} /> */}
-    {/* <Resource
-      name="users"
-      list={UserList}
-      show={ShowGuesser}
-      recordRepresentation="name"
-      icon={UserIcon}
-    /> */}
+    <Resource name="/" list={UserList} icon={UserIcon} />
   </Admin>
 );
